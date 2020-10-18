bfb1a8a9132f3421cc09f77ed62e09b1b0cef77c
Sarina Lyons <sarinalyons@gmail.com>
2020-10-18 18:49:43 -0400

    updated Python101 Exercise

---
+++ Python101/Medium-Exercises/ex5.py
@@ -0,0 +1,16 @@
+##variables/row set up
+
+row_1 = 5 * (" ") + "*"
+row_2 = 4 * (" ") + 3 * ("*")
+row_4 = 3 * (" ") + 5 * ("*")
+row_5 = 2 * (" ") + 7 * ("*")
+
+##print statement
+print(row_1)
+print(row_2)
+print(row_4)
+print(row_5)
+
+
+
+
