929cba931d651187dfdd8043265faf3b39e2b6ab
Sarina Lyons <sarinalyons@gmail.com>
2020-11-04 00:08:05 -0500

    updates

---
+++ JavaScript-LP-101/Medium-Exercises/ex8.js
@@ -0,0 +1,36 @@
+let subs = ["A", "E", "G", "I", "O", "S", "T"];
+
+function leetSpeak(text) {
+    subs.forEach(function(value){
+        switch (sub) {
+        case "A":
+            console.log("4");
+            break;
+        case "E":
+            console.log("3");
+            break;
+        case "G":
+            console.log(6);
+            break;
+        case "I":
+            console.log(1);
+            break;
+        case "O":
+            console.log(0);
+            break;
+        case "S":
+            console.log(5);
+            break;
+        case "T":
+            console.log(T);
+            break;
+        default:
+            console.log(null);
+        }
+    return text;
+  }
+})
+
+
+leetSpeak("Leet");
+
