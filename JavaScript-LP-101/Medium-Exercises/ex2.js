77282e7db4ede15b2ec491d16033065ac43bc311
Sarina Lyons <sarinalyons@gmail.com>
2020-11-04 10:32:12 -0500

    updates

--- JavaScript-LP-101/Medium-Exercises/ex2.js
+++ JavaScript-LP-101/Medium-Exercises/ex2.js
@@ -1,5 +1,5 @@
-//set holder for total
-let total;
+// //set holder for total
+// let total;
 
 //create object for service tip values
 let myService = {
@@ -8,21 +8,33 @@ let myService = {
   bad: 0.1,
 };
 
-//define function for calculations
+// //define function for calculations
+// function tipAmount(billAmount, service) {
+//   for (attrib in myService) {
+//     if (service === "good") {
+//       total = myService.good * billAmount;
+//     } else if (service === "fair") {
+//       total = myService.fair * billAmount;
+//     } else {
+//       total = myService.bad * billAmount;
+//     }
+//   }
+//   return total;
+// }
+
+//other option
 function tipAmount(billAmount, service) {
+  let total = 0.2 * billAmount;
   for (attrib in myService) {
-    if (service === "good") {
-      total = myService.good * billAmount;
-    } else if (service === "fair") {
-      total = myService.fair * billAmount;
-    } else {
-      total = myService.bad * billAmount;
+    if (attrib == service) {
+      total = myService[attrib] * billAmount;
     }
   }
   return total;
 }
+
 //call function
-tipAmount(100, "good");
+let total = tipAmount(100, "good");
 
 //print result for test visibility
 console.log(total);
